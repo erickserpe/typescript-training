62a267585da163c27618cdb3e953518495be3df9
erick <serpetabordaerickluan@gmail.com>
2024-09-22 14:41:46 -0300

    Pesquisa de metodos de arrendondamento

---
+++ src/listaPOO2/arredondamento.ts
@@ -0,0 +1,11 @@
+/*Exercício 9
+ARREDONDAMENTO – Pesquise, teste e diferencie os métodos de Math: ceil(), floor(),
+round() e trunc().*/
+
+
+let num: number = 4.567;
+
+console.log(`Math.ceil(${num}) = ${Math.ceil(num)}`);   // Arredonda para cima
+console.log(`Math.floor(${num}) = ${Math.floor(num)}`); // Arredonda para baixo
+console.log(`Math.round(${num}) = ${Math.round(num)}`); // Arredonda para o mais próximo
+console.log(`Math.trunc(${num}) = ${Math.trunc(num)}`); // Remove a parte decimal
